a1976d2ceaa0de7b2e3bf0c33f2bd21bc38e2209
quartz-cha <quartzchacha@gmail.com>
2026-03-10 19:36:55 +0900

    リリース日の修正

--- static/data/versions/v1_0_0.ts
+++ static/data/versions/v1_0_0.ts
@@ -2,6 +2,6 @@ import { VersionInfo } from "../../../src/types";
 
 export const v1_0_0: VersionInfo = {
   "version": "v1.0.0",
-  "date": "2026/01/26",
+  "date": "2026/03/09",
   "detail": "最初のバージョンをリリースしました。"
 }
