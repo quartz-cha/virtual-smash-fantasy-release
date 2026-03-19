7c837edd60ad03a2baf79d30fe6be4402ab380da
quartz-cha <quartzchacha@gmail.com>
2026-03-19 19:42:24 +0900

    サービス開始

--- static/data/news/index.ts
+++ static/data/news/index.ts
@@ -1,8 +1,10 @@
 import { News } from "../../../src/types";
 import { abdhiudbcakiici } from "./abdhiudbcakiici";
 import { eryftvgedfwiuofh } from "./eryftvgedfwiuofh";
+import { jufwiejsnjnfoeifje } from "./jufwiejsnjnfoeifje";
 
 export const index: News[] = [
+  jufwiejsnjnfoeifje,
   eryftvgedfwiuofh,
   abdhiudbcakiici
 ];
---
+++ static/data/news/jufwiejsnjnfoeifje.ts
@@ -0,0 +1,11 @@
+import { News } from "../../../src/types";
+
+export const jufwiejsnjnfoeifje: News = {
+  "id": "jufwiejsnjnfoeifje",
+  "date": "2026/03/19",
+  "title": "サービスを開始しました",
+  "abstruct": "本日より、ゲームをプレイできるようになりました。",
+  "detail": "本日より、ゲームをプレイできるようになりました。<br>ダウンロード済みの方は、ゲームを起動すればすぐに遊べます。<br>大乱闘の世界をお楽しみください！",
+  "tags": [],
+  "files": []
+}
